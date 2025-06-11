2d995fc7e48c0342a3caabb9107b99237ced4fa1
My-school-account-ig <27olrees@sjhs.newport.sch.uk>
2025-06-11 20:31:42 +0100

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -6,9 +6,33 @@ const boardWidth = columnCount*tileSize;
 const boardHeight = rowCount*tileSize;
 let context;
 
+let blueGhostImage;
+let orangeGhostImage;
+let pinkGhostImage;
+let redGhostImage;
+let pacmanUpImage;
+let pacmanDownImage;
+let pacmanLeftImage;
+let pacmanRightImage;
+let wallImage;
+
 window.onload = function() {
   board = document.getElementById("board");
   board.height = boardHeight;
   board.width = boardWidth;
   context = board.getContext("2d")
+}
+
+function loadImages() {
+  wallImage = new Image();
+  wallImage.src = "wall.pmg";
+
+  blueGhostImage = new Image();
+  blueGhostImage.src = "blueGhost.png";
+  orangeGhostImage = new Image();
+  orangeGhostImage.src = "orangeGhost.png";
+  pinkGhostImage = new Image();
+  pinkGhostImage.src = "pinkGhost.png";
+  redGhostImage = new Image();
+  redGhostImage.src = "redGhost.png";
 }
